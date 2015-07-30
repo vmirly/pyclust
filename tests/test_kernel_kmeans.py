12926b1ecd2839842ee45917eeb8c237f065c0bd
mirjalil <vmirjalily@gmail.com>
2015-07-30 09:36:09 -0400

    Kernel Kmeans: correcting the test-file

--- tests/test_kernel_kmeans.py
+++ tests/test_kernel_kmeans.py
@@ -6,40 +6,37 @@ d1 = np.random.uniform(low=0, high=2, size=(10,2))
 d2 = np.random.uniform(low=3, high=5, size=(10,2))
 d = np.vstack((d1,d2))
 
-print(d)
-print(d.shape)
-kg = pyclust._kernel_kmeans._compute_gram_matrix(d, sigma_sq=2.0)
 
-print(kg.shape)
+def test_kernelkmeans():
+    print(d)
+    print(d.shape)
+    kg = pyclust._kernel_kmeans._compute_gram_matrix(d, kern_type='rbf', sigma_sq=2.0)
 
-print(kg[0,:])
-print(kg[19,:])
+    print(kg.shape)
 
-w1 = np.random.randint(2, size=20)
-w2 = 1 - w1
-wmemb_old = np.vstack((w1,w2)).T
+    print(kg[0,:])
+    print(kg[19,:])
 
-
-#print(wmemb_old.shape)
-#print(wmemb_old)
-
-for it in range(1):
-  wmemb_new = np.zeros(shape=(20,2), dtype=int)
-  kdist = np.empty(shape=(20,2), dtype=float)
-  for i in range(20):
-     kdist[i] = pyclust._kernel_kmeans._kernelized_dist2centers(kg, wmemb_old, i, 0)
-     wmemb_new[i,np.argmin(kdist[i])] = 1
-
-  #print(kdist)
-  #print(wmemb_new)
-
-  wmemb_old = wmemb_new.copy()
+    w1 = np.random.randint(2, size=20)
+    w2 = 1 - w1
+    wmemb_old = np.vstack((w1,w2)).T
 
 
-import sklearn
-import sklearn.metrics.pairwise 
+    #print(wmemb_old.shape)
+    #print(wmemb_old)
 
-pk = sklearn.metrics.pairwise.pairwise_kernels(d, metric='rbf')
-print(pk[0,:])
-print(pk[19,:])
-print(pk.shape)
+    for it in range(1):
+        wmemb_new = np.zeros(shape=(20,2), dtype=int)
+        kdist = np.empty(shape=(20,2), dtype=float)
+        for i in range(20):
+            kdist[i] = pyclust._kernel_kmeans._kernelized_dist2centers(kg, wmemb_old, i)
+            wmemb_new[i,np.argmin(kdist[i])] = 1
+
+        #print(kdist)
+        #print(wmemb_new)
+
+        wmemb_old = wmemb_new.copy()
+
+
+
+test_kernelkmeans()
