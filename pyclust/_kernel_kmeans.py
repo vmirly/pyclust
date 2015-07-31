816feb9fbb90f800640a2f4efb19c521bfb4d5ef
Vahid <vmirjalily@gmail.com>
2015-07-31 12:08:53 -0400

    Kernel Kmeans fixed

--- pyclust/_kernel_kmeans.py
+++ pyclust/_kernel_kmeans.py
@@ -5,13 +5,20 @@ from . import _kmeans as kmeans
 
 
 
-def _compute_gram_matrix(X, kern_type, sigma_sq=2.0):
+def _compute_gram_matrix(X, kernel_type, params):
     """
     """
-    if kern_type == 'rbf':
+    if kernel_type == 'rbf':
+        if 'gamma' in params:
+            gamma = params['gamma']
+        else:
+            gamma = 1.0 / X.shape[1]
+
         pairwise_dist = scipy.spatial.distance.pdist(X, metric='sqeuclidean')
         pairwise_dist = scipy.spatial.distance.squareform(pairwise_dist)
-        gram_matrix = np.exp( - pairwise_dist / sigma_sq)
+        gram_matrix = np.exp( - gamma * pairwise_dist )
+
+        np.fill_diagonal(gram_matrix, 1)
     else:
         pass
 
@@ -41,20 +48,27 @@ def _kernelized_dist2centers(K, n_clusters, wmemb, kernel_dist):
         kernel_dist[:,j] -= 2 * np.sum(K[:, memb_j], axis=1) / size_j
 
 
+    return
 
-def _fit_kernelkmeans(K, n_clusters, max_iter):
+
+def _fit_kernelkmeans(K, n_clusters, max_iter, converge_tol=0.01):
     """
     """
     n_samples = K.shape[0]
-    labels_old = np.random.randint(n_clusters, size=n_samples)
+    membs_prev = np.random.randint(n_clusters, size=n_samples)
     kdist = np.empty(shape=(n_samples, n_clusters), dtype=float)
     for it in range(max_iter):
         kdist.fill(0)
-        _kernelized_dist2centers(K, n_clusters, labels_old, kdist)
-        labels = np.argmin(kdist, axis=1)
+        _kernelized_dist2centers(K, n_clusters, membs_prev, kdist)
 
+        membs_curr = np.argmin(kdist, axis=1)
+        membs_changed_ratio = float(np.sum((membs_curr - membs_prev) != 0)) / n_samples
+        if membs_changed_ratio < converge_tol:
+            break
 
-    return(labels)
+        membs_prev = membs_curr
+
+    return(it, membs_curr)
 
 
 
@@ -62,25 +76,23 @@ class KernelKMeans(object):
     """
     """
 
-    def __init__(self, n_clusters=2, kernel_type='linear', n_trials=10, max_iter=100):
+    def __init__(self, n_clusters=2, kernel='linear', params={}, n_trials=10, max_iter=100):
         assert (kernel in ['linear', 'rbf'])
         self.n_clusters  = n_clusters
-        self.kernel_type = kernel_type
+        self.kernel_type = kernel
         self.n_trials    = n_trials
         self.max_iter    = max_iter
+
+        self.kernel_params = params
 
     def fit(self, X):
         """
         """
-        self.kernel_matrix_ = _compute_gram_matrix(X, self.kernel_type)
-        self.labels_ = _fit_kernelkmeans(self.kernel_matrix_, self.n_clusters, self.max_iter)
-        self.converged = True
+        self.kernel_matrix_ = _compute_gram_matrix(X, self.kernel_type, self.kernel_params)
+        self.n_iter_, self.labels_ = _fit_kernelkmeans(self.kernel_matrix_, self.n_clusters, self.max_iter)
 
     def fit_predict(self, X):
         """
         """
-        if not self.converged:
-            raise("Model is not fit yet!")
-
         self.fit(X)
         return(self.labels_)
--- tests/test_kernel_kmeans.py
+++ tests/test_kernel_kmeans.py
@@ -2,15 +2,14 @@ import numpy as np
 import pyclust
 
 
-d1 = np.random.uniform(low=0, high=2, size=(100,2))
-d2 = np.random.uniform(low=3, high=5, size=(100,2))
+d1 = np.random.uniform(low=0, high=3, size=(20,2))
+d2 = np.random.uniform(low=3, high=5, size=(20,2))
 d = np.vstack((d1,d2))
-
 from scipy.sparse import issparse
 
 
 def test_kernelkmeans():
-    kg = pyclust._kernel_kmeans._compute_gram_matrix(d, kern_type='rbf', sigma_sq=2.0)
+    kg = pyclust._kernel_kmeans._compute_gram_matrix(d, kernel_type='rbf', params={})
 
     #print(kg[0,:])
     #print(kg[19,:])
@@ -20,21 +19,11 @@ def test_kernelkmeans():
 
     kdist = np.zeros(shape=(20,2), dtype=float)
 
-    print(pyclust._kernel_kmeans._fit_kernelkmeans(kg, 2, 100))
-    return
+    res = pyclust._kernel_kmeans._fit_kernelkmeans(kg, 2, 100)
 
-    for it in range(10):
-
-        kdist = np.empty(shape=(20,2), dtype=float)
-        pyclust._kernel_kmeans._kernelized_dist2centers(kg, 2, wmemb_old, kdist)
-        wmemb_new = np.argmin(kdist, axis=1)
-
-        #print(kdist)
-        #print(wmemb_new)
-
-        wmemb_old = wmemb_new.copy()
-
-    print(kdist)
-    print(wmemb_new)
+    kkm = pyclust.KernelKMeans(n_clusters=2, kernel='rbf')
+    kkm.fit_predict(d)
+    print("Converged after %d iterations"%kkm.n_iter_)
+    print(kkm.labels_)
 
 test_kernelkmeans()
