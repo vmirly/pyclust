440c5bb077bb1a82ed47df61f5488f30fca0a97a
Vahid <vmirjalily@gmail.com>
2015-08-13 15:48:19 -0400

    Silhouette: intra-cluster distances

--- pyclust/validate/_internal.py
+++ pyclust/validate/_internal.py
@@ -16,7 +16,38 @@ def _cal_silhouette_score(X, y, sample_size, metric):
     n_clust_samp = len(np.unique(y_samp))
     pair_dist = scipy.spatial.distance.pdist(X_samp, metric=metric)
 
-    return(pair_dist)
+    pair_dist_matrix = scipy.spatial.distance.squareform(pair_dist)
+
+    ## Compute average intra-cluster distances 
+    ## for each of the selected samples
+    a_arr = _intra_cluster_distances(pair_dist_matrix, y_samp, np.arange(y_samp.shape[0]))
+
+
+    return(a_arr)
+
+
+def _intra_cluster_distances(dist_matrix, y, ilist):
+    """
+    """
+    n_samples = y.shape[0]
+    if type(ilist) == type(list()):
+        n_inx = len(ilist)
+    elif type(ilist) == type(np.array([])):
+        n_inx = ilist.shape[0]
+    else:
+        raise Exception("ilist must be iterable!")
+
+
+    mean_intra_distances = np.empty(shape=n_inx, dtype=float)
+
+    for i,inx in enumerate(ilist):
+        mask = y == y[inx]
+        if np.sum(mask)>0:
+            mean_intra_distances[i] = np.mean(dist_matrix[mask].T[inx])
+        else:
+            mean_intra_distances[i] = 0.0
+
+    return(mean_intra_distances) 
 
 class Silhouette(object):
     """
